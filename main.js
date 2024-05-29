1aafc42178dc9a3eaf4170c14bfb892d691d8190
efrezel <jademillan726@gmail.com>
2024-05-29 20:26:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+        document.body.classList.remove("container");
+
+        const audio = new Audio('Souvenir (320).mp3');
+        audio.play();
+    
+    
+};
+    
+        
+
+
+ 
---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    const audio = new Audio('Souvenir (320).mp3');
+    audio.play();
+
+
+};
+
+onload = () =>{
+    document.body.classList.remove("container");
+};
+
